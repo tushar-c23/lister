59656ead46fec472534684a154b6a80109b42979
Tushar Choudhary <tushar.choudhary@syfe.com>
2025-02-08 18:54:50 +0530

    Init

---
+++ src/main/kotlin/com/damodar/ranklist/RanklistApplication.kt
@@ -0,0 +1,11 @@
+package com.damodar.ranklist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RanklistApplication
+
+fun main(args: Array<String>) {
+	runApplication<RanklistApplication>(*args)
+}
---
+++ src/test/kotlin/com/damodar/ranklist/RanklistApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.damodar.ranklist
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RanklistApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
